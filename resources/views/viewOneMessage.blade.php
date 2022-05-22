47570f2a01885544381e1ff55cb10116ea12cdef
Shahed1998 <shahedchowdhury120@gmail.com>
2022-05-23 00:31:53 +0600

    ...

--- resources/views/viewOneMessage.blade.php
+++ resources/views/viewOneMessage.blade.php
@@ -3,12 +3,23 @@
     @include('includes.dashNav')
     <div class="desc editProfile">
         <h1 class="d-flex justify-content-center align-items-center shadow p-5 mb-5 bg-body rounded">Messages</h1>
-        <div class="w-50 mx-auto">
+        <div class="message ms-3">
+            <label for="name">Name:</label><br>
+            <input type="text" class="w-75" name="" id="" disabled value="{{$mailInfo->name}}"><br>
+            <label for="sub">Subject:</label><br>
+            <input type="text" class="w-75" name="" id="sub" disabled value="{{$mailInfo->subject}}"><br>
+            <label for="comment">Comment:</label><br>
+            <textarea name="" id="" class="w-75" cols="30" rows="10" disabled >{{$mailInfo->comment}}</textarea>
+        </div>
+        <h2 class="d-flex justify-content-center align-items-center shadow p-5 mb-5 bg-body rounded">Reply</h2>
+
+    </div>
+@endsection
+
+<!-- <div class="w-50 mx-auto">
+            <label for="name">Name:</label><br>
+            <input type="text" name="" size="26" id="name" disabled value="{{$mailInfo->name}}">
             <table>
-                <tr>
-                    <td><label for="name">Name:</label></td>
-                    <td><input type="text" name="" size="26" id="name" disabled value="{{$mailInfo->name}}"></td>
-                </tr>
                 <tr>
                     <td><label for="email">Email:</label></td>
                     <td><input type="text" name="" id="email" size="26" disabled value="{{$mailInfo->email}}"></td>
@@ -22,6 +33,4 @@
                     <td><textarea name="" id="" cols="30" rows="10" disabled >{{$mailInfo->comment}}</textarea></td>
                 </tr>
             </table>
-        </div>
-    </div>
-@endsection
+        </div> -->
